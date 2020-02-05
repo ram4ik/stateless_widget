3d13e49336039dec1f620d4b5fe28793968ae3d5
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-05 10:52:48 +0200

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyFirstApp());
 
 
-class MyFirstApp extends StatelessWidget {
-  
+class MyFirstApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO:
+    return _MyFirstAppState();
+  }
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  bool _loading;
+  double _progressValue;
+
+  @override
+  void initState() {
+    _loading = false;
+    _progressValue = 0.0;
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,26 +37,48 @@ class MyFirstApp extends StatelessWidget {
         body: Center(
           child: Container(
             padding: EdgeInsets.all(16),
-            child: Column(
+            child: _loading ?
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                LinearProgressIndicator(value: 23),
+                LinearProgressIndicator(value: _progressValue),
                 Text(
-                  '23 %', style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                Text(
-                  'Press button to download', style: TextStyle(color: Colors.white, fontSize: 20),
+                  '${(_progressValue * 100).round()}%', style: TextStyle(color: Colors.white, fontSize: 20),
                   ),
               ],
+            )
+                : Text(
+              'Press button to download', style: TextStyle(color: Colors.white, fontSize: 20),
             ),
           ),
         ),
 
         floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
           child: Icon(Icons.cloud_download),
           ),
       ),
     );
+  }
+
+  void _updateProgress() {
+    const onSec = const Duration(seconds: 1);
+    Timer.periodic(onSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.2;
+
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
   }
 }
